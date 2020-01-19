459938b4eb09f36b73c2cc44d833f457ea2fce86
Annie <anniezheng.anniee@gmail.com>
2020-01-19 14:07:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()) + 1
+const mod = 18 % 7
+const max = Math.max(0, 20, 3, 6)
