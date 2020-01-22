ab49837003a95032defff9382968764c7281ceeb
Soner Mezgitci <you@example.com>
2020-01-22 10:19:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let var1 = 31
-let var2 = 2 
-let multiply = var1 * var2;
+let num1 = 31
+let num2 = 2 
+let multiply = num1 * num2;
 let random = (Math.random() * 10) 
 let max = Math.max(5, 10, 15, 20)
 let mod = (4 % 10)
-// Write your code here
