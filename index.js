82953a39296a99591c88ac356c2d4908f41a6d11
ajshemi <akpojotor@yahoo.com>
2020-04-03 23:42:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1=62
+let num2=1
+let counter=16
+let set=[1,2,6,20]
+const multiply=num1*num2
+
+const random= Math.floor(Math.random() * 10) + 1;
+
+const mod = counter % 6
+
+
+const max=Math.max(...set)
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,5 @@
 const chai = require('chai')
 const expect = chai.expect;
-
 describe('index.js', function() {
   describe('multiply', function() {
     it("is an equation whose multiplied results will equal 62", function() {
